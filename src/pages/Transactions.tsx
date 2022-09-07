baacf3204d0b96cbd18d3356187f39a663f3e2d2
lucas <lac.lucas96@gmail.com>
2022-09-07 19:10:55 -0300

    feat: create transactions table

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -6,6 +6,32 @@ export const Transactions = () => {
 		<>
 			<Header />
 			<Summary />
+			<main className="max-w-screen-xl mx-auto px-4 xl:px-0 my-16">
+				<table className="table-auto border-separate border-spacing-x-0 border-spacing-y-2 w-full">
+					<tbody>
+						<tr>
+							<td className="px-8 py-5 bg-gray-700 rounded-tl-md rounded-bl-md">
+								Desenvolvimento de site
+							</td>
+							<td className={`px-8 py-5 bg-gray-700 text-green-300`}>R$ 12.000,00</td>
+							<td className="px-8 py-5 bg-gray-700">Vendas</td>
+							<td className="px-8 py-5 bg-gray-700 rounded-tr-md rounded-br-md">
+								13/04/2022
+							</td>
+						</tr>
+						<tr>
+							<td className="px-8 py-5 bg-gray-700 rounded-tl-md rounded-bl-md">
+								Compras
+							</td>
+							<td className="px-8 py-5 bg-gray-700 text-red-300">- R$ 50,00</td>
+							<td className="px-8 py-5 bg-gray-700">Alimentação</td>
+							<td className="px-8 py-5 bg-gray-700  rounded-tr-md rounded-br-md">
+								13/04/2022
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</main>
 		</>
 	);
 };
