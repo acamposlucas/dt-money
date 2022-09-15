d3918cd9e537143566ec4c199f563c90cd2724c5
lucas <lac.lucas96@gmail.com>
2022-09-15 09:39:53 -0300

    feat: searchForm filter

--- src/pages/Transactions/components/SearchForm.tsx
+++ src/pages/Transactions/components/SearchForm.tsx
@@ -2,6 +2,8 @@ import { MagnifyingGlass } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../contexts/TransactionContext";
 
 const searchFormSchema = z.object({
 	query: z.string(),
@@ -10,6 +12,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export const SearchForm = () => {
+	const { fetchTransactions } = useContext(TransactionsContext);
+
 	const {
 		register,
 		handleSubmit,
@@ -19,9 +23,7 @@ export const SearchForm = () => {
 	});
 
 	async function handleSearchTransactions(data: SearchFormInputs) {
-		await new Promise((resolve) => setTimeout(resolve, 2000));
-
-		console.log(data);
+		await fetchTransactions(data.query);
 	}
 	return (
 		<form
